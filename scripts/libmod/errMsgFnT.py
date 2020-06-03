591f618ac82d7fdac158e3dcb4a6d0d173a396bd
s.nandi <56621007+snandiDS@users.noreply.github.com>
2020-06-03 15:20:11 +0530

    Create errMsgFnT.py

---
+++ scripts/libmod/errMsgFnT.py
@@ -0,0 +1,9 @@
+def fileErrMsg():
+   fileErrorMsg = """
+                    File not created or found!
+                    Please check if the file exists.
+                    Please check the permission of the directory and give permission.
+                 """
+
+   print(fileErrorMsg)
+   return 1 
